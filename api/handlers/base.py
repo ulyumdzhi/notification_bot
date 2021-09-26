42efaf95751243ba465334f748c5638645cb2bb1
ulyumdzhi <ulyumdzhiev@gmail.com>
2021-09-26 21:43:33 +0300

    base handler update

--- api/handlers/base.py
+++ api/handlers/base.py
@@ -1,3 +1,4 @@
+import re
 import time
 import logging
 
@@ -5,7 +6,7 @@ from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Command
 
-from api.loader import bot, dp
+from api.loader import bot, dp, ds_admin, js_admin
 from api.utils import static_text as st
 from api.utils import keyboard as kb
 from states.user_data import UserData as ud
@@ -17,6 +18,7 @@ async def not_target(message: types.Message):
     text = st.NOT_TARGET_TEXT %user_name
     await message.answer(text)
 
+
 @dp.message_handler(Command("help"), state="*")
 async def send_help(message: types.Message):
     """Обработка сообщения о помощи"""
@@ -26,7 +28,8 @@ async def send_help(message: types.Message):
 
     logging.info(f"Help alarm from id={user_id} named {user_fullname} at {time.strftime('%X %x')}")
 
-    await message.answer(f"{st.HELP_TEXT}" %user_name, reply_markup=kb.secondMenu)
+    await message.answer(f"{st.HELP_TEXT}" %user_name, reply_markup=kb.main_menu)
+
 
 @dp.message_handler(Command("start"), state="*")
 async def send_welcome(message: types.Message):
@@ -37,8 +40,9 @@ async def send_welcome(message: types.Message):
 
     logging.info(f"Start massage from id={user_id} named {user_fullname} at {time.strftime('%X %x')}")
 
-    await message.answer(f"{st.HELLO}" %user_name, reply_markup=kb.mainMenu)
-    await ud.came_to.set()
+    await message.answer(f"{st.HELLO}" %user_name, reply_markup=kb.hello_menu)
+    await ud.info.set()
+
 
 @dp.message_handler(text='Начать заново', state="*")
 async def restart(message: types.Message):
@@ -47,6 +51,41 @@ async def restart(message: types.Message):
     user_id = message.from_user.id   
 
     logging.info(f"ReStart massage from id={user_id} named {user_fullname} at {time.strftime('%X %x')}")
+
+    await send_welcome(message)
+
+
+@dp.message_handler(state=ud.info)
+async def answer_q0(message: types.Message, state: FSMContext):
+    """Нулевой вопрос. 
+    Согласие на обработку данных"""
+    
+    user_name = message.from_user.first_name
+    user_fullname = message.from_user.full_name
+    user_id = message.from_user.id    
+    answer = message.text
+
+    logging.info(f"from {user_name} permission {answer} at {time.strftime('%X %x')}")
+
+    await state.update_data(info=answer)
+
+    await message.answer(f"{st.INFO}", reply_markup=kb.dsjs_menu)
+
+    await state.update_data(user_id=user_id)
+    await state.update_data(user_name=user_fullname)
+    await ud.came_to.set()
+
+
+@dp.message_handler(text='Даю согласие!', state=ud.info)
+async def restart(message: types.Message):
+    """Обработка согласия бота"""
+
+    await send_welcome(message)
+
+
+@dp.message_handler(text='Не даю согласие!', state=ud.info)
+async def restart(message: types.Message):
+    """Обработка согласия бота"""
 
     await send_welcome(message)
     
