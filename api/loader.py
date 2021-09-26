8df5d45d38f2d7c97a877a8225cb37656ef4634e
ulyumdzhi <ulyumdzhiev@gmail.com>
2021-09-26 19:48:49 +0300

    env and config

--- api/loader.py
+++ api/loader.py
@@ -6,23 +6,31 @@ from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-# # вместо dotenv можно использовать config.py и подгружать все токены в виде статических переменных
-# from config import * 
+# вместо dotenv можно использовать config.py и подгружать все токены в виде статических переменных
+from api.utils.config import * 
 
 # Включаем логирование
 logging.basicConfig(filename='log.log', 
                     encoding='utf-8',
                     level=logging.INFO)
 
-# Подгружаем переменные окружения
+# # Подгружаем переменные окружения, например из файла .env
 load_dotenv()
 
 # Telegram API токен @BotFather
-TOKEN = os.getenv("BOT_TOKEN")
+try:
+    TOKEN = BOT_TOKEN
+except:
+    TOKEN = os.getenv("BOT_TOKEN")
 
 # Admin ID - те сотрудники, кому будут приходить оповещения, что гость уже на месте
-ds_admin = os.getenv("DS_ADMIN_ID")
-js_admin = os.getenv("JS_ADMIN_ID")
+try:
+    ds_admin = DS_ADMIN_ID
+    js_admin = JS_ADMIN_ID
+except:
+    ds_admin = os.getenv("DS_ADMIN_ID")
+    js_admin = os.getenv("JS_ADMIN_ID")
+    
 
 # Инициализируем bot, dispatcher и storage
 storage = MemoryStorage()
@@ -30,11 +38,11 @@ bot = Bot(token=TOKEN, parse_mode=types.ParseMode.MARKDOWN_V2)
 dp = Dispatcher(bot, storage=storage)
 
 # Подгружаем данные для соединения с Google Sheets 
-spreadsheet_id = os.getenv("TABLE_ID")
-CREDENTIALS_FILE = "notification_bot/spreadsheet/credentials.json" # Google Developer Console 
+try:
+    spreadsheet_id = TABLE_ID
+except:
+    spreadsheet_id = os.getenv("TABLE_ID")
+
+CREDENTIALS_FILE = 'spreadsheet/credentials.json' # Google Developer Console 
 
 logging.info(f"Start of bot at {time.strftime('%X %x')}")
-
-
-
-
